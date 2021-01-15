2d2c363a9582941463ef210b93490a6ffb60085d
Neil Alexander <nalexa200@caledonian.ac.uk>
2021-01-15 22:50:51 +0000

    Event Types Added
    - Added different match events.
    - Created a method to print full match event with type, coordinate, time.

--- Assets/Scripts/MatchEventTrigger.cs
+++ Assets/Scripts/MatchEventTrigger.cs
@@ -18,10 +18,27 @@ public class MatchEventTrigger : MonoBehaviour
     [SerializeField]
     Text eventInfoText;
 
+    [SerializeField]
+    Text eventWithType;
+
+    [SerializeField]
+    GameObject eventRegisteredPanel;
+
+    [SerializeField]
+    GameObject viewEventsList;
+
+    /*
+    public string[] eventArray;
+    int i = 0;
+    */
+
+    List<string> eventList = new List<string>();
+
     void Start()
     {
         gameUI.SetActive(true);
         matchEventUI.SetActive(false);
+        viewEventsList.SetActive(false);
     }
 
     public void OnClicked (Button button)
@@ -35,14 +52,39 @@ public class MatchEventTrigger : MonoBehaviour
         eventStopClock.StopTimer();
 
         gameUI.SetActive(false);
+        eventRegisteredPanel.SetActive(true);
+        viewEventsList.SetActive(false);
         matchEventUI.SetActive(true);
 
         eventInfoText.text = "Event Coordinate: " + buttonCoordinate + " Time: " + printStopwatchTime.GetComponent<Text>().text;
     }
 
+    public void AddEventType (string eventType)
+    {
+        viewEventsList.SetActive(true);
+        eventRegisteredPanel.SetActive(false);
+
+        eventWithType.text = "(" + eventType + ") " + eventInfoText.text;
+
+        eventList.Add(eventWithType.text);
+
+        /*
+        eventArray[i] = eventWithType.text;
+        i++;
+        */
+    }
+
     public void returnToGame()
     {
         gameUI.SetActive(true);
+        viewEventsList.SetActive(false);
+        eventRegisteredPanel.SetActive(false);
         matchEventUI.SetActive(false);
+
+        eventList.ForEach(print);
+       /* for (int x = 0; x < eventArray.Length; x++)
+        {
+            print(eventArray[x].ToString());
+        }*/
     }
 }
