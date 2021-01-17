1f9e838d0ddecb24bf10654c67f10f9f2bea5e57
Neil Alexander <nalexa200@caledonian.ac.uk>
2021-01-17 17:49:50 +0000

    Implemented Match Events List Exported to txt file
    - Added new script handling exporting of match events list.
    - Game now outputs a txt file when player presses new "Match Complete" button.

---
+++ Assets/Scripts/GenerateTextFile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class GenerateTextFile : MonoBehaviour
+{
+    [SerializeField]
+    Text finalEventsList;
+
+    [SerializeField]
+    Text ingameAllEventsList;
+
+    public void CreateFileOfEvents()
+    {
+        string path = Application.dataPath + "/MatchLog.txt";
+
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "Match Log \n\n");
+        }
+
+        File.WriteAllText(path, finalEventsList.text);
+
+        ingameAllEventsList.text = finalEventsList.text;
+
+    }
+}
