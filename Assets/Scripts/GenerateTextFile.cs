c41b03565a1b0bfdd7380fdc3012ca0a1d0ae9b0
Neil Alexander <nalexa200@caledonian.ac.uk>
2021-01-18 14:35:35 +0000

    Added Match Data to Txt File Output
    - Txt file generated after a match ends now also saves the match data generated during the match.
    - Match Complete now presents a screen of all match events, followed by the final match data screen.

--- Assets/Scripts/GenerateTextFile.cs
+++ Assets/Scripts/GenerateTextFile.cs
@@ -12,6 +12,37 @@ public class GenerateTextFile : MonoBehaviour
     [SerializeField]
     Text ingameAllEventsList;
 
+    // Variables below for final match data values
+    [SerializeField]
+    Text shotsForText;
+
+    [SerializeField]
+    Text shotsAgainstText;
+
+    [SerializeField]
+    Text turnoversText;
+
+    [SerializeField]
+    Text turnoversAgainstText;
+
+    [SerializeField]
+    Text penPassesText;
+
+    [SerializeField]
+    Text penPassesAgainstText;
+
+    [SerializeField]
+    Text PPRText;
+
+    [SerializeField]
+    Text PPRagainstText;
+
+    [SerializeField]
+    Text crossesForText;
+
+    [SerializeField]
+    Text crossesAgainstText;
+
     public void CreateFileOfEvents()
     {
         string path = Application.dataPath + "/MatchLog.txt";
@@ -21,7 +52,27 @@ public class GenerateTextFile : MonoBehaviour
             File.WriteAllText(path, "Match Log \n\n");
         }
 
-        File.WriteAllText(path, finalEventsList.text);
+        // Add match events to content string
+        string content = "All Match Events:" + "\n\n" + finalEventsList.text + "\n\n";
+
+        // Add match data to string
+        content += "Match Data:" + "\n\n";
+
+        content += "Shots For:          " + shotsForText.text + "\n";
+        content += "Turnovers For:      " + turnoversText.text + "\n";
+        content += "Crosses For:        " + crossesForText.text + "\n";
+        content += "PP For:             " + penPassesText.text + "\n";
+        content += "PP Received For:    " + PPRText.text + "\n";
+
+        content += "\n";
+
+        content += "Shots Against:         " + shotsAgainstText.text + "\n";
+        content += "Turnovers Against:     " + turnoversAgainstText.text + "\n";
+        content += "Crosses Against:       " + crossesAgainstText.text + "\n";
+        content += "PP Against:            " + penPassesAgainstText.text + "\n";
+        content += "PP Received Against:   " + PPRagainstText.text + "\n";
+
+        File.WriteAllText(path, content);
 
         ingameAllEventsList.text = finalEventsList.text;
 
